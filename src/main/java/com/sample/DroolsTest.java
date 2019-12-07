04b950fba16747b2869f347de6e79a3d55eac5fb
jansmi <janek.smielowski@gmail.com>
2019-12-07 14:44:23 +0100

    Rebuilt init

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -3,24 +3,32 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import javax.swing.JOptionPane;
 
-/**
- * This is a sample class to launch a rule.
- */
 public class DroolsTest {
 
-    public static final void main(String[] args) {
+	public static final void main(String[] args) {
         try {
-            // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
-        	KieSession kSession = kContainer.newKieSession("ksession-rules");
-
-            // go !
+            KieSession kSession = kContainer.newKieSession("ksession-rules");
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
+    
+    public static int askQuestion(String question, String[] answers) {
+      int n = JOptionPane.showOptionDialog(null,
+          question,
+          null,
+          JOptionPane.YES_NO_OPTION,
+          JOptionPane.QUESTION_MESSAGE,
+          null,
+          answers,
+          answers[0]);
+      
+      return n;
+  }
 
 }
